55a2c277277bf0ef0c9e7563ae784d1519c5c8e2
anthony <jauch-anthony@hotmail.fr>
2021-07-24 17:15:50 +0200

    add FireBaseMessagingService

---
+++ app/src/main/java/com/jammy/notification/MyFirebaseMessagingService.java
@@ -0,0 +1,118 @@
+package com.jammy.notification;
+
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.jammy.R;
+import com.jammy.scene.dashboard.DashboardActivity;
+
+import java.util.Random;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public MyFirebaseMessagingService() {
+    }
+
+
+    private static final String TAG = "mFirebaseIIDService";
+    private static final String SUBSCRIBE_TO = "/topics/TestTopic";
+    private final String ADMIN_CHANNEL_ID ="admin_channel";
+    private String token;
+
+    @Override
+    public void onNewToken(String newToken) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        super.onNewToken(newToken);
+        token = newToken;
+        Log.d(TAG, "Refreshed token: " + token);
+
+    }
+
+    public void onTokenRefresh() {
+        onTokenRefresh();
+        /*
+          This method is invoked whenever the token refreshes
+          OPTIONAL: If you want to send messages to this application instance
+          or manage this apps subscriptions on the server side,
+          you can send this token to your server.
+        */
+        Task<String> token = FirebaseMessaging.getInstance().getToken();
+
+        // Once the token is generated, subscribe to topic with the userId
+        FirebaseMessaging.getInstance().subscribeToTopic(SUBSCRIBE_TO);
+        Log.i(TAG, "onTokenRefresh completed with token: " + token);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        final Intent intent = new Intent(this, DashboardActivity.class);
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        int notificationID = new Random().nextInt(3000);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            setupChannels(notificationManager);
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_notif);
+
+        Uri notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notif)
+                .setColor(ContextCompat.getColor(MyFirebaseMessagingService.this, R.color.colorPrimary))
+                .setLargeIcon(largeIcon)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setAutoCancel(true)
+                .setSound(notificationSoundUri)
+                .setContentIntent(pendingIntent);
+
+        //Set notification color to match your app color template
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            notificationBuilder.setColor(getResources().getColor(R.color.colorPrimary));
+        }
+        notificationManager.notify(notificationID, notificationBuilder.build());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels(NotificationManager notificationManager) {
+        CharSequence adminChannelName = "New notification";
+        String adminChannelDescription = "Device to devie notification";
+
+        NotificationChannel adminChannel;
+        adminChannel = new NotificationChannel(ADMIN_CHANNEL_ID,adminChannelName,NotificationManager.IMPORTANCE_HIGH);
+        adminChannel.setDescription(adminChannelDescription);
+        adminChannel.enableLights(true);
+        adminChannel.setLightColor(Color.RED);
+        adminChannel.enableVibration(true);
+        if (notificationManager != null){
+            notificationManager.createNotificationChannel(adminChannel);
+        }
+    }
+}
+
+
